28d0f4c2ec29932a565928bdfb812b0516ccf5a0
Juan029 <juadcobo1@misena.edu.co>
2024-07-18 12:54:11 -0500

    Agregamos a Query y Mutation la cita

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -2,6 +2,7 @@ const { gql } = require('apollo-server')
 
 module.exports = gql `
 type Recipe{
+    id: ID!
     name: String
     lastname: String
     age: Int
@@ -10,6 +11,16 @@ type Recipe{
     thumbsDown: Int
 }
 
+
+type CitaMedica {
+    id: ID!
+    fecha: String
+    motivo: String
+    persona: Recipe
+}
+
+
+
 input RecipeInput{
     name: String
     lastname: String
@@ -17,9 +28,19 @@ input RecipeInput{
 }
 
 
+input CitaMedicaInput {
+    fecha: String
+    motivo: String
+    personaId: ID!
+}
+
+
+
 type Query{
     recipe(ID: ID!): Recipe!
     getRecipes(amount: Int): [Recipe]
+    citaMedica(ID: ID!): CitaMedica!
+    getCitasMedicas: [CitaMedica]
 }
 
 
@@ -27,5 +48,6 @@ type Mutation {
     createRecipe(recipeInput: RecipeInput): Recipe!
     deleteRecipe(ID: ID!): Boolean
     editRecipe(ID: ID!, recipeInput: RecipeInput): Boolean
+    createCitaMedica(citaMedicaInput: CitaMedicaInput): CitaMedica
 }
 `
