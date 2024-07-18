383f6fecdbd424d7ce931e03e9a91c6aa7c3054b
Juan029 <juadcobo1@misena.edu.co>
2024-07-18 16:12:38 -0500

    Ya funciona todo

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -6,76 +6,64 @@ module.exports = {
         async recipe(_, {ID}){
             return await Recipe.findById(ID)
         },
-
         async getRecipes(_, {amount}){
             return await Recipe.find().sort({createdAt: -1}).limit(amount)
         },
-
         async citaMedica(_, {ID}){
             return await CitaMedica.findById(ID)
         },
-
-        async getCitasMedicas(){
-            return await CitaMedica.find()
-        }
-
-
+        async getCitasMedicas(_, { personaId }) {
+            if (personaId) {
+              return await CitaMedica.find({ personaId });
+            }
+            return await CitaMedica.find();
+          }
     },
     Mutation: {
-        async createRecipe(_, {recipeInput: {name,lastname,age}}){
+        async createRecipe(_, {recipeInput: {name, lastname, age}}){
             const createdRecipe = new Recipe({
-                name: name,
-                lastname: lastname,
-                age: age,
+                name,
+                lastname,
+                age,
                 createdAt: new Date().toISOString(),
-                thumbsUp:0,
-                thumbsDown:0
+                thumbsUp: 0,
+                thumbsDown: 0
             });
 
-            const res = await createdRecipe.save(); // save at mongo
-
+            const res = await createdRecipe.save();
             console.log(res._doc);
 
             return {
                 id: res.id,
                 ...res._doc
             }
-         
         },
         async deleteRecipe(_, {ID}){
            const wasDeleted = (await Recipe.deleteOne({_id: ID})).deletedCount
-           return wasDeleted; //1 si elimina o 0 sino
+           return wasDeleted; // 1 si elimina o 0 sino
         },
-        async editRecipe(_, {ID, recipeInput: {name, lastname}}){
-            const wasEdited = (await Recipe.updateOne({_id: ID}, {name:name, lastname: lastname, age: age})).modifiedCount;
-            return wasEdited; //1 si ha sido modificado o 0 sino 
-  
-
-
-            
+        async editRecipe(_, {ID, recipeInput: {name, lastname, age}}){
+            const wasEdited = (await Recipe.updateOne({_id: ID}, {name, lastname, age})).modifiedCount;
+            return wasEdited; // 1 si ha sido modificado o 0 sino 
         },
+        async createCitaMedica(_, {citaMedicaInput: {fecha, motivo, personaId}}) {
+            const createdCitaMedica = new CitaMedica({
+                fecha,
+                motivo,
+                personaId
+            });
 
-        //Cita mutation
-    
-            async createCitaMedica(_, {citaMedicaInput: {fecha, motivo, personaId}}) {
-                const createdCitaMedica = new CitaMedica({
-                    fecha,
-                    motivo,
-                    personaId
-                });
-    
-                const res = await createdCitaMedica.save();
-    
-                return {
-                    id: res.id,
-                    ...res._doc
-                }
-            }
-        },
-        CitaMedica: {
-            async persona(citaMedica) {
-                return await Recipe.findById(citaMedica.personaId)
+            const res = await createdCitaMedica.save();
+
+            return {
+                id: res.id,
+                ...res._doc
             }
         }
+    },
+    CitaMedica: {
+        async persona(citaMedica) {
+            return await Recipe.findById(citaMedica.personaId)
+        }
     }
- 
+}
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -14,23 +14,24 @@ type Recipe{
 
 type CitaMedica {
     id: ID!
-    fecha: String
-    motivo: String
-    persona: Recipe
+    personaId: ID!
+    fecha: String!
+    motivo: String!
+    persona: Recipe!
 }
 
 
 
 input RecipeInput{
-    name: String
-    lastname: String
-    age: Int
+    name: String!
+    lastname: String!
+    age: Int!
 }
 
 
 input CitaMedicaInput {
-    fecha: String
-    motivo: String
+    fecha: String!
+    motivo: String!
     personaId: ID!
 }
 
@@ -40,14 +41,14 @@ type Query{
     recipe(ID: ID!): Recipe!
     getRecipes(amount: Int): [Recipe]
     citaMedica(ID: ID!): CitaMedica!
-    getCitasMedicas: [CitaMedica]
+    getCitasMedicas(personaId: ID): [CitaMedica]!
 }
 
 
 type Mutation {
     createRecipe(recipeInput: RecipeInput): Recipe!
     deleteRecipe(ID: ID!): Boolean
-    editRecipe(ID: ID!, recipeInput: RecipeInput): Boolean
-    createCitaMedica(citaMedicaInput: CitaMedicaInput): CitaMedica
+    editRecipe(ID: ID!, recipeInput: RecipeInput!): Boolean
+    createCitaMedica(citaMedicaInput: CitaMedicaInput!): CitaMedica!
 }
 `
